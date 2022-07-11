940580abb44cd27a09bc24a003f504584c977f2a
faisolrr <faisolrizkiromdon@gmail.com>
2022-07-12 01:16:09 +0700

    collect assignments

---
+++ src/component/home/Home.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Home.css';
+import doctorStrange from '../asset/doctor-strange.jpg'
+
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div className='container'>
+                <ul>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                    <li>
+                        <a href=' '><img src={doctorStrange} alt=""/><br/>Doctor Strange <br/>in the Multiverse of<br/>Madness</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Home
