8fe652b55a71496882964760011679cd3ce7af9f
faisolrr <faisolrizkiromdon@gmail.com>
2022-07-13 20:20:46 +0700

    memanggil api dengan axios

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,7 +17,7 @@ class Card2 extends Component {
   render() {
     return (
       <div className="p-3 flex flex-col justify-between bg-neutral-500 rounded shadow-lg shadow-black">
-        <img src={this.props.image} alt={this.props.title} height="750" />
+        <img src={this.props.image ? `https://image.tmdb.org/t/p/original${this.props.image}` : "https://via.placeholder.com/500x750?text=No+Image"} alt={this.props.title} height="750" />
         <p className="text-center text-white font-bold">{this.props.title}</p>
         <Button label="Add to favorite" />
       </div>
@@ -25,7 +25,4 @@ class Card2 extends Component {
   }
 }
 
-// 1. export default
-// export default Card;
-// 2. named export
 export { Card, Card2 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card2 } from "../components/Card";
 import Header from "../components/Header";
+import axios from "axios";
 
 class HomePage extends Component {
   // constructor
@@ -8,38 +9,7 @@ class HomePage extends Component {
     title: "-",
     content: "This is the home page",
     page: 1,
-    datas: [
-      {
-        id: 1,
-        title: "Wandavision Season 1",
-        image: "https://cdn.europosters.eu/image/750/posters/wandavision-reality-rift-i106807.jpg",
-      },
-      {
-        id: 2,
-        title: "Wandavision Season 2",
-        image: "https://image.tmdb.org/t/p/original/glKDfE6btIRcVB5zrjspRIs4r52.jpg",
-      },
-      {
-        id: 3,
-        title: "Wandavision Season 3",
-        image: "https://cdn.europosters.eu/image/750/posters/wandavision-reality-rift-i106807.jpg",
-      },
-      {
-        id: 4,
-        title: "Wandavision Season 4 Wandavision Season 4 Wandavision Season 4",
-        image: "https://cdn.europosters.eu/image/750/posters/wandavision-reality-rift-i106807.jpg",
-      },
-      {
-        id: 5,
-        title: "Wandavision Season 5",
-        image: "https://image.tmdb.org/t/p/original/glKDfE6btIRcVB5zrjspRIs4r52.jpg",
-      },
-      {
-        id: 6,
-        title: "Wandavision Season 6",
-        image: "https://cdn.europosters.eu/image/750/posters/wandavision-reality-rift-i106807.jpg",
-      },
-    ],
+    datas: [],
     information: {},
     loading: false,
   };
@@ -51,12 +21,18 @@ class HomePage extends Component {
 
   // ini fungsi yang dijalankan ketika component dimuat
   async fetchData() {
-    // setTimeout(() => {
-    this.setState({
-      title: "Home Test",
-    });
-    console.log(this.state.title);
-    // }, 2000);
+    this.setState({ loading: true });
+    await axios
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+      .then((response) => {
+        const { results } = response.data;
+        console.log(results);
+        this.setState({ datas: results });
+      })
+      .catch((error) => {
+        alert(error.toString());
+      })
+      .finally(() => this.setState({ loading: false }));
   }
 
   render() {
@@ -67,7 +43,7 @@ class HomePage extends Component {
           <p>{this.state.content}</p>
           <div className="grid grid-flow-row auto-rows-max grid-cols-2 md:grid-cols-4 lg:grid-cols-5 m-2 gap-3">
             {this.state.datas.map((data) => (
-              <Card2 key={data.id} title={data.title} image={data.image} />
+              <Card2 key={data.id} title={data.title} image={data.poster_path} />
             ))}
           </div>
         </div>
